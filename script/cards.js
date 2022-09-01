4d9361e18949787ca2404e1c5bd03b2272f84307
Sergei Zakharchenko <millennial404@yandex.ru>
2022-09-02 02:37:15 +0300

    fix: review 1

---
+++ script/cards.js
@@ -0,0 +1,26 @@
+const initialCards = [
+    {
+      name: 'Москва',
+      link: 'https://images.unsplash.com/photo-1641152185456-c916bc799257?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=3264&q=80'
+    },
+    {
+      name: 'Санкт-Петербург',
+      link: 'https://images.unsplash.com/photo-1628533447434-f2f4b344bd46?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2274&q=80'
+    },
+    {
+      name: 'Красная поляна',
+      link: 'https://images.unsplash.com/photo-1547925972-ad174a37ac60?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2832&q=80'
+    },
+    {
+      name: 'Сочи',
+      link: 'https://images.unsplash.com/photo-1549092156-04ee20673b6e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1335&q=80'
+    },
+    {
+      name: 'Екатеринбург',
+      link: 'https://images.unsplash.com/photo-1568183113672-bfc2f6c66198?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=987&q=80'
+    },
+    {
+      name: 'Казань',
+      link: 'https://images.unsplash.com/photo-1631775866694-fe340840cc52?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2274&q=80'
+    }
+    ];
