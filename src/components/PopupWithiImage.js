f95687bca6fb3c6c6412cb8612434e0b7b91f175
abrakadabra <annapetrova13@ya.ru>
2023-02-10 15:04:02 +0300

    add PopupWithiImage

---
+++ src/components/PopupWithiImage.js
@@ -0,0 +1,18 @@
+import Popup from "./Popup";
+
+export default class PopupWithImage extends Popup {
+  constructor(selectorPopup) {
+    super(selectorPopup);
+    this.image = this.popup.querySelector('.popup__img');
+    this.imageCaption = this.popup.querySelector('.popup__img-caption');
+
+  }
+
+  open(linkValue, nameValue) {
+    super.open();
+    this.image.setAttribute('src', linkValue);
+    this.image.setAttribute('alt', nameValue);
+    this.imageCaption.textContent = nameValue;
+  }
+
+}
